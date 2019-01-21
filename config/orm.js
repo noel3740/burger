c50daa447f1dedf9b7555d1a7ab64542ce15e1b8
Noel Nevarez <noel3740@gmail.com>
2019-01-21 09:32:25 -0600

    Correcting initial coding for orm

--- config/orm.js
+++ config/orm.js
@@ -1,33 +1,22 @@
+//Add dependencies
 const connection = require('./connection');
 
-//Connection to the database
-connection.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
+var orm = {
+    //Function to select all from the burger table
+    selectAll: (tableName) => {
+        console.log("this is a test");
+    },
+
+    //Function to insert one burger
+    insertOne: (tableName) => {
+        console.log("this is a test2");
+    },
+
+    //Function to update one burger
+    updateOne: (tableName) => {
+        console.log("this is a test3");
     }
-  
-    console.log("connected as id " + connection.threadId);
-  });
+}
 
-  //Function to select all from the burger table
-  function selectAll() {
-      console.log("this is a test");
-  }
-
-  //Function to insert one burger
-  function insertOne() {
-    console.log("this is a test2");
-  }
-
-  //Function to update one burger
-  function updateOne() {
-    console.log("this is a test3");
-  }
-
-  //Export the functions
-  module.exports = { 
-      selectAll,
-      insertOne,
-      updateOne
-    };
+//Export the functions
+module.exports = orm;
