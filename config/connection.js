ec60bad97f8bcfa4883bdba7046825ce5d63c7db
Noel Nevarez <noel3740@gmail.com>
2019-01-21 09:29:43 -0600

    Adding host name as a .env config item. Moving connect method.

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
     onnection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: "localhost",
+        host: process.env.DATABASE_HOST,
 
         // Your port; if not 3306
         port: 3306,
@@ -21,6 +21,16 @@ if (process.env.JAWSDB_URL) {
         database: "burgers_db"
     });
 }
+
+//Connection to the database
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+
+    console.log("connected as id " + connection.threadId);
+});
 
 //Export the connection
 module.exports = connection;
