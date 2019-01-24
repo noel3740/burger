ba3d9f3265a447dd392cdb5c92fd4f52b8458262
Noel Nevarez <noel3740@gmail.com>
2019-01-23 20:12:02 -0600

    Adding required field validity.

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -3,21 +3,26 @@ $(document).ready(() => {
         //Prevent the screen from being refreshed on submit
         event.preventDefault();
 
-        //Create a new burger object
-        var newBurger = {
-            burger_name: this.burgerName.value.trim(),
-            devoured: 0
-        };
+        //Check for errors in required fields and prevent post to API if there are errors. 
+        if (this.checkValidity()) {
+            //Create a new burger object
+            var newBurger = {
+                burger_name: this.burgerName.value.trim(),
+                devoured: 0
+            };
 
-        //Send an ajax post request to create the new burger
-        $.ajax("/api/burgers", {
-            type: "POST",
-            data: newBurger
-        }).then(function () {
-            console.log("Created new burger");
-            //Reload the page to get the updated burger list on the screen
-            location.reload();
-        });
+            //Send an ajax post request to create the new burger
+            $.ajax("/api/burgers", {
+                type: "POST",
+                data: newBurger
+            }).then(function () {
+                console.log("Created new burger");
+                //Reload the page to get the updated burger list on the screen
+                location.reload();
+            });
+        }
+
+        this.classList.add('was-validated');
     });
 
     $(".devourButton").on("click", function (event) {
